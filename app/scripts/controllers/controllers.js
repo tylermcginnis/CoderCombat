4e1d470dc7f60bda9b719ee3abbaa6489666697f
Tyler McGinnis <tylermcginnis33@gmail.com>
2013-09-17 10:33:07 -0700

    add logo, minor style changes

--- app/scripts/controllers/controllers.js
+++ app/scripts/controllers/controllers.js
@@ -1,11 +1,8 @@
 angular.module('CoderCombatApp.controllers', [])
   .controller('mainCntrl', ['$scope', 'socket', '$modal', function ($scope, socket, $modal) {
-    $scope.challenge = 'Using the JavaScript language, have the function LetterChanges(str) 
-    take the str parameter being passed and modify it using the following algorithm. 
-    Replace every letter in the string with the letter following it in the alphabet 
-    (ie. c becomes d, z becomes a).Then capitalize every vowel in this new string 
-    (a, e, i, o, u) and finally return this modified string. ';
     
+    $scope.challenge = "Using the JavaScript language, have the function LetterChanges(str) take the str parameter being passed and modify it using the following algorithm. Replace every letter in the string with the letter following it in the alphabet (ie. c becomes d, z becomes a).Then capitalize every vowel in this new string (a, e, i, o, u) and finally return this modified string.";
+
     socket.on('join', function (room) {
         socket.emit('init', room);
     });
