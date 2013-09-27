fad4d9cce290930b9e51212fe9073e11896100db
Tyler McGinnis <tylermcginnis33@gmail.com>
2013-09-26 21:27:57 -0700

    Still having some async issues but it I throw in some things to delay events it works perfectly. BASE

--- app/scripts/controllers/controllers.js
+++ app/scripts/controllers/controllers.js
@@ -10,14 +10,16 @@ angular.module('CoderCombatApp.controllers', [])
       var parameter;
       httpConnect.connect()
           .success(function(data, status){
-            alert('successful connection');
+            console.log('HttpConnection Made')
             questionObj = data;
           }).error(function(data, status){
             console.log("An error occured on httpConnect");
           });
+
       socket.on('join', function (room) {
           socket.emit('init', room);
       });
+
       socket.on('oppDisconnect', function(){
         $modal({
           template: '../../views/disconnect-modal.html',
@@ -25,6 +27,7 @@ angular.module('CoderCombatApp.controllers', [])
           scope: $scope
         });
       });
+
       socket.on('modalStart', function(){
         $modal({
           template: '../../views/pairing-modal.html',
@@ -32,10 +35,10 @@ angular.module('CoderCombatApp.controllers', [])
           scope: $scope
         });
       });
+
       socket.on('modalEnd', function(rand){
         if(rand){
           questionObj[1].rnd = rand;
-          console.log('new', questionObj);
           socket.emit('sendQuestion', questionObj);
         } else{
           questionObj[1].rnd = undefined;
@@ -45,18 +48,19 @@ angular.module('CoderCombatApp.controllers', [])
           $scope.$modal('hide');
         }
       });
+
       socket.on('updateQuestion', function(questionObj){
-        console.log('this is the question recieved on updateQuestion', questionObj);
         //Don't judge me for what I'm about to do, it will all be over soon.
         $('#coding-challenge-header').text(questionObj.title);
         $('#problem').text(questionObj.challenge);
         //It's all over. No more DOM manipulation from the Cntrl using jQuery. Promise.
 
-          $scope.challenge = questionObj.challenge;
-          $scope.title = questionObj.title;
-          answer = questionObj.answer;
-          parameter = questionObj.parameter;
+        $scope.challenge = questionObj.challenge;
+        $scope.title = questionObj.title;
+        answer = questionObj.answer;
+        parameter = questionObj.parameter;
       });
+
       socket.on('loserModal', function(room){
         $modal({
           template: '../../views/loser-modal.html',
@@ -71,6 +75,7 @@ angular.module('CoderCombatApp.controllers', [])
   return input;\n\
 }"
 //ignore weird structure above. Needed for Ace Editor.
+
         var editor = ace.edit('leftEditor');
         var userAnswer = editor.getSession().getValue();
         userAnswer = userAnswer.split("");
@@ -79,23 +84,22 @@ angular.module('CoderCombatApp.controllers', [])
         userAnswer = userAnswer.join("");
         var result = eval(userAnswer);
         if(result === answer){
-            //reset editors
-            socket.emit('editorAfterSubmit', originalContent);
+          //reset editors
+          socket.emit('editorAfterSubmit', originalContent);
 
-            var timeOut = setTimeout(function(){
-              socket.emit('anotherMatch');
-            }, 10000);
+          var timeOut = setTimeout(function(){
+            socket.emit('anotherMatch');
+          }, 10000);
 
-            //tell the person who lost
-            socket.emit('youLost');
+          //tell the person who lost
+          socket.emit('youLost');
 
-           //person who submitted it, congratuations modal
-            $modal({
-              template: '../../views/congratulations-modal.html',
-              keyboard: false,
-              scope: $scope
-            }); 
-
+         //person who submitted it, congratuations modal
+          $modal({
+            template: '../../views/congratulations-modal.html',
+            keyboard: false,
+            scope: $scope
+          }); 
         } else { 
           alert('Looks like you didn\'t have the correct answer. Try again.');
         }
--- server.js
+++ server.js
@@ -59,11 +59,7 @@ var numOfUndefines = 0;
 
 io.sockets.on('connection', function (socket) {
   console.log(socket.id +  ' CONNECTED');
-  console.log('Room list -62', roomList);
-  console.log('real rooms 63', io.sockets.manager.rooms);
-  
   initcount += 1;
-  console.log('new initcount - 64: ', initcount);
   if(numOfUndefines === 0){
     if(initcount % 2 === 1){
       roomcount += 1;
@@ -73,11 +69,9 @@ io.sockets.on('connection', function (socket) {
       roomList[room].user2 = socket.id;
     }
     socket.join(room);
-    console.log(socket.id + 'just joined 74 room: ' + room);
     socket['room'] = room;
     socket.in(room).emit('join', room);
   } else if(numOfUndefines > 0 && numOfUndefines % 2 === 1){
-    console.log('this is shown because numOfUndefines > 0 and odd')
     var disconnectedRoom;
     for(var property in roomList){
       if(roomList[property].user1 === 0){
@@ -85,39 +79,30 @@ io.sockets.on('connection', function (socket) {
         roomList[disconnectedRoom].user1 = socket.id;
         socket.join(disconnectedRoom);
         numOfUndefines--;
-        console.log('numOfUnd is now - 86: ',numOfUndefines);
         if(numOfUndefines < 0){
           numOfUndefines = 0;
         }
-        console.log('numOfUndefines is now - 90',numOfUndefines);
         socket.broadcast.emit('modalEnd');
       } else if(roomList[property].user2 === 0){
           disconnectedRoom = property;
           roomList[disconnectedRoom].user2 = socket.id;
           socket.join(disconnectedRoom);
-          console.log(socket.id + ' just joined room - 96: ' + disconnectedRoom);
           numOfUndefines--;
-          console.log('numOfUndefines is now - 98',numOfUndefines);
           if(numOfUndefines < 0){
             numOfUndefines = 0;
           }
-          console.log('numOfUndefines is now - 102',numOfUndefines);
           socket.broadcast.emit('modalEnd');
       }
     }
-      console.log('Room list -102', roomList);
-      console.log('real rooms 103', io.sockets.manager.rooms);
   } 
 
   socket.on('initialEditor', function(editorText){
     var room = socket['room'];
     socket.broadcast.to(room).emit('setInitialVals', editorText);
-    console.log('initialEditor was called on room -113: ', room);
   });
 
   socket.on('editorAfterSubmit', function(text){
     var room = socket['room'];
-    console.log('editorAfterSubmit about to be called in room', room);
     io.sockets.in(room).emit('setInitialVals', text);
   });
 
@@ -129,9 +114,7 @@ io.sockets.on('connection', function (socket) {
   socket.on('sendQuestion', function(questionObj){
     var theQuestion = {};
     var room = socket['room'];
-    console.log('the roomList 132', roomList);
     if(questionObj[1].rnd !== undefined){
-      console.log("THE ROOM 134", room);
       if(roomList[room]){
         roomList[room].rnd = questionObj[1].rnd;
       }
@@ -140,18 +123,15 @@ io.sockets.on('connection', function (socket) {
     if(roomList[room]){
       if(roomList[room].rnd === undefined){
         roomList[room].rnd = Math.floor(Math.random() * questionObj.length-1) + 1;
-        console.log("THIS IS THE SECOND RANDOM NUMBER! NO LOG : ", roomList[room].rnd);
       }
     }
 
-    console.log('roomList after', roomList);
     if(roomList[room]){
       if(roomList[room].rnd){
         theQuestion.title = questionObj[roomList[room].rnd].title;
         theQuestion.challenge = questionObj[roomList[room].rnd].question;
         theQuestion.parameter = questionObj[roomList[room].rnd].parameter;
         theQuestion.answer = questionObj[roomList[room].rnd].answer;
-        console.log("This is the question title being sent to updateQuestion", theQuestion.title);
       
         //only send the final quesiton to updateQuestion
         io.sockets.in(room).emit('updateQuestion', theQuestion);
@@ -164,24 +144,15 @@ io.sockets.on('connection', function (socket) {
     if (initcount % 2 === 0){
         console.log('Initialize New Match in room ' , room);
         io.sockets.in(room).emit('modalEnd');
-        console.log('modalEnd was just emitted to room - 130: ', room);
-        console.log('Room list -131', roomList);
-        console.log('real rooms 132', io.sockets.manager.rooms);
-        console.log('init count - 133', initcount);
       } else if(initcount % 2 === 1){
           console.log("Waiting for an opponent in room ", room);
           io.sockets.in(room).emit('modalStart');
-          console.log('modalStart was just emitted to room - 137: ', room);
-          console.log('Room list -138', roomList);
-          console.log('real rooms 139', io.sockets.manager.rooms);
-          console.log('initcount - 140', initcount);
       }
   });
 
   socket.on('youLost', function(room){
     room = socket['room'];
     socket.broadcast.to(room).emit('loserModal');
-    console.log('loserModal was just emmited to -147 room: ', room);
   });
 
   socket.on('anotherMatch', function(){
@@ -191,9 +162,7 @@ io.sockets.on('connection', function (socket) {
   });
 
   socket.on('disconnect', function (){
-    console.log(socket.id + ' Disconnected');
     initcount -=1;
-    console.log('initcount just became', 153);
     var disconUser = socket.id;
     //for loop sets the disconnected user to 0 in roomList
     for (var prop in roomList) {
@@ -201,38 +170,30 @@ io.sockets.on('connection', function (socket) {
           roomList[prop].user1 = 0;
           room = prop;
           numOfUndefines++;
-          console.log('numOfUndefines just became - 161',numOfUndefines);
         } else if(roomList[prop].user2 === disconUser){
           roomList[prop].user2 = 0
           room = prop;
           numOfUndefines++;
-          console.log('numOfUndefines just became - 166',numOfUndefines);
         }
+
         if(roomList[prop].user1 === 0 && roomList[prop].user2 === 0){
           numOfUndefines -= 2
-          console.log('numofUnd just got subtracted by two to be 170-: ', numOfUndefines);
           if(numOfUndefines < 0){
             numOfUndefines = 0;
           }
-          console.log('numOfUndefines just became- 173',numOfUndefines);
           delete roomList[prop];
           roomcount--;
-          console.log('roomcount just became', roomcount);
         }
     }
-    console.log('Room list -180', roomList);
-    console.log('real rooms 181', io.sockets.manager.rooms);
+
     socket.broadcast.to(room).emit('oppDisconnect');
-    console.log('oppDisconnect was just emmitted to 183 room: ', room);
 
     if(initcount === 0){
       numOfUndefines = 0;
-      console.log('numOfUndefines became 0 because initcount was 0 - 187');
     } 
 
     //two people with disconnected partners should be paired
     if(numOfUndefines % 2 === 0 && numOfUndefines > 0){
-      console.log('this was logged because numOfUndefines was even and > 0');
       var firstDisconnectedRoom;
       var firstDisconnectedUser;
       var secondDisconnectPartner;
@@ -261,45 +222,28 @@ io.sockets.on('connection', function (socket) {
           }
         }
 
-          console.log('after loop, firstDisconnectedUser: 221:', firstDisconnectedUser);
-          console.log('firstDisconnectedRoom 222:', firstDisconnectedRoom);
-          console.log('secondDisconnectPartner 223', secondDisconnectPartner);
-          console.log('secondDisconnectRoom 224', secondDisconnectRoom);
-
         var socketNew = secondDisconnectPartner;
         if(socketNew){
           if(firstDisconnectedUser === 'user1'){
             roomList[firstDisconnectedRoom].user1 = secondDisconnectPartner.id;
             delete roomList[secondDisconnectRoom];
             roomcount--;
-            console.log('roomcount just became 232', roomcount);
           } else if(firstDisconnectedUser === 'user2'){
             roomList[firstDisconnectedRoom].user2 = secondDisconnectPartner.id;
             delete roomList[secondDisconnectRoom];
             roomcount--;
-            console.log('roomcount just became 237', roomcount);
           }
 
           socketNew.leave(secondDisconnectRoom);
-          console.log(socketNew.id + 'just left ' + secondDisconnectRoom);
           socketNew.join(firstDisconnectedRoom);
-          console.log(socketNew.id + 'just joined 243' + firstDisconnectedRoom);
           socketNew.room = firstDisconnectedRoom;
-          // io.sockets.in(firstDisconnectedRoom).emit('modalEnd'); //CHANGED THIS LINE
           numOfUndefines -= 2;
-          console.log('numOfUndefines just got subtracted by 2 to be250', numOfUndefines);
           if(numOfUndefines < 0){
             numOfUndefines = 0;
-            console.log('numOfUndefines just became 0 since it was negative, 249', numOfUndefines)
           }
-          // io.sockets.in(firstDisconnectedRoom).emit('updateQuestion',questionObj);
-          // io.sockets.in(firstDisconnectedRoom).emit('setInitialVals', editorText);
-          console.log('Room list -233', roomList);
-          console.log('real rooms 234', io.sockets.manager.rooms);
-          // socket.broadcast.emit('modalEnd');
+
           var random = Math.floor(Math.random() * 10-1) + 1;
-          io.sockets.in(firstDisconnectedRoom).emit('modalEnd', random); //CHANGED THIS LINE
-          console.log('modalEnd was just emitted to 256', firstDisconnectedRoom);
+          io.sockets.in(firstDisconnectedRoom).emit('modalEnd', random);
         }
       }, 500);
     }
@@ -307,78 +251,64 @@ io.sockets.on('connection', function (socket) {
     //guard on if a player disconnects before getting a partner
     if(initcount === 0){
       numOfUndefines = 0;
-      console.log('265 numOfUndefines just became 0 since initcount was 0. this should never be logged');
     } 
+
     if(roomList[socket['room']]){
       if(Object.keys(roomList[socket['room']]).length === 1 && numOfUndefines === 1){
         numOfUndefines = 0
-        console.log(' 270 numOfUndefines just became 0 since it was the only room and the only numOfUndefines');
       }
     };
 
     //if player is looking for a partner when someone quits, pair new gamer with old widow
     if(initcount % 2 === 0 && numOfUndefines === 1){
-      console.log('276 this was logged because initcount is even and numOfUndefines was 1');
       var widowRoom;
       var widower;
       var newPlayer;
       var go = true;
       setTimeout(function(){
-      var newPlayerRoom;
-      for(var roomNum in roomList){
-        while(go){
-          if(roomList[roomNum].user1 === 0){
-            widowRoom = roomNum;
-            widower = 'user1';
-            go = false;
-          } else if(roomList[roomNum].user2 === 0){
-            widowRoom = roomNum;
-            widower = 'user2';
-            go = false;
+        var newPlayerRoom;
+        for(var roomNum in roomList){
+          while(go){
+            if(roomList[roomNum].user1 === 0){
+              widowRoom = roomNum;
+              widower = 'user1';
+              go = false;
+            } else if(roomList[roomNum].user2 === 0){
+              widowRoom = roomNum;
+              widower = 'user2';
+              go = false;
+            }
+          }
+
+          if(roomList[roomNum]){
+            if(Object.keys(roomList[roomNum]).length === 1){
+              newPlayerRoom = roomNum;
+              newPlayer = io.sockets.clients(newPlayerRoom)[0];
+              newPlayer.leave(newPlayerRoom);
+              newPlayer.join(widowRoom);
+              newPlayer.room = widowRoom;
+              
+              //match roomList to new room
+              if(widower === 'user1'){
+                  roomList[widowRoom].user1 = newPlayer.id;
+                  delete roomList[newPlayerRoom];
+                  roomcount--;
+                } else if(widower === 'user2'){
+                  roomList[widowRoom].user2 = newPlayer.id;
+                  delete roomList[newPlayerRoom];
+                  roomcount--;
+                }
+
+              numOfUndefines && (numOfUndefines--);
+              var random = Math.floor(Math.random() * 10-1) + 1;
+              io.sockets.in(widowRoom).emit('modalEnd', random);
+              break;
+            }
           }
         }
-        console.log('widowRoom 295', widowRoom);
-        console.log('widower 296', widower);
-
-        if(roomList[roomNum]){
-          if(Object.keys(roomList[roomNum]).length === 1){
-            newPlayerRoom = roomNum;
-            console.log('305 newPlayerRoom', newPlayerRoom);
-            newPlayer = io.sockets.clients(newPlayerRoom)[0];
-            console.log('306 newPlayer id', newPlayer.id);
-            newPlayer.leave(newPlayerRoom);
-            console.log(newPlayer.id + 'just left ' + newPlayerRoom);
-            newPlayer.join(widowRoom);
-            console.log(newPlayer.id + 'just joined 307' + widowRoom);
-            newPlayer.room = widowRoom;
-            
-            //match roomList to new room
-            if(widower === 'user1'){
-                roomList[widowRoom].user1 = newPlayer.id;
-                delete roomList[newPlayerRoom];
-                roomcount--;
-                console.log('roomcount became 315', roomcount);
-              } else if(widower === 'user2'){
-                roomList[widowRoom].user2 = newPlayer.id;
-                delete roomList[newPlayerRoom];
-                roomcount--;
-                console.log('roomcount became 320', roomcount);
-              }
-
-            numOfUndefines && (numOfUndefines--);
-            console.log('numOfUndefines just became- 324',numOfUndefines);
-            var random = Math.floor(Math.random() * 10-1) + 1;
-            io.sockets.in(widowRoom).emit('modalEnd', random);
-            console.log('modalEnd was just emitted to 326', widowRoom);
-            console.log('Room list -327', roomList);
-            console.log('real rooms 328', io.sockets.manager.rooms);
-            break;
-          }
-        }
-       }
       }, 500);
     }
-    socket.leave(room); //added. try removing later. 
+    socket.leave(room); 
   });
 });
 
